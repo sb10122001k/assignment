3ea548bdc67cc2a8bd1acbd5248cbf40b4a2a877
Shubham <shubhamppt1170@gmail.com>
2023-09-08 20:40:20 +0530

    hoooo

---
+++ api/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { v4 } from "uuid";
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8080;
+
+httpServer.listen(port, "0.0.0.0", () => {
+  console.log(`Now listening on port ${port}`);
+});
+
+app.get("/api/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/calculate", (req, res) => {
+  try {
+    const data = req.body;
+    let result;
+
+    // Convert num1 and num2 to numbers
+    const num1 = Number(data.num1);
+    const num2 = Number(data.num2);
+
+    switch (data.op) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      default:
+        throw new Error("Invalid operation");
+    }
+
+    res.json({ result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
